4eef18f3e395bcff8e379fed2acc0ff6fa78cfa0
Cinea Zhan <cinea@cinea.com.cn>
2023-07-05 15:08:46 +0800

    暂存框架

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
 import './assets/application.css'
 
 import {createApp} from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import App from './App.vue'
 import HomePage from '@/components/HomePage.vue'
 import CustomerLogin from '@/components/customer/CustomerLogin.vue'
-import axios from "axios";
+import axios from 'axios'
 import 'element-plus/theme-chalk/src/message.scss'
 
 // 配置axios
-if (import.meta.env.MODE === "development")
-  axios.defaults.baseURL = "http://localhost:5212/";
-else
-  axios.defaults.baseURL = "https://cinema.cinea.com.cn/";
+if (import.meta.env.MODE === 'development') axios.defaults.baseURL = 'http://localhost:5212/'
+else axios.defaults.baseURL = 'https://cinema.cinea.com.cn/'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
-    {path: '/', component: HomePage},
-    {path: '/login', component: CustomerLogin}
+    { path: '/', component: HomePage },
+    { path: '/login', component: CustomerLogin }
   ]
 })
 
