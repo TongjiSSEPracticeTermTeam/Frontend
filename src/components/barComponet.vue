93dfde885fe6573a692341f36b9d135ef606fb99
Raymond <raymond4ever@163.com>
2023-07-07 16:40:09 +0800

    提交电影管理页面

---
+++ src/components/barComponet.vue
@@ -0,0 +1,75 @@
+
+
+<script>
+export default {
+    props: {
+        movieName: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        releaseDate: {
+            type: String,
+            required: true
+        },
+        endDate: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="bar-component">
+        <div class="data-item name">{{ movieName }}</div>
+        <div class="data-item duration">{{ duration }}分钟</div>
+        <div class="data-item releaseDate">{{ releaseDate }}</div>
+        <div class="data-item endDate">{{ endDate }}</div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.bar-component {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    width: 100%;
+
+    background-color: #f0f0f0;
+}
+
+.data-item {
+    /* border: 3px solid black; */
+    font-size: 1em;
+    color: #999;
+    margin: 0;
+}
+.data-item.name {
+    color: orange;
+    font-style: bolder;
+    width: 30%;
+    /* background-color: red; */
+}
+.data-item.duration {
+    /* background-color: blue; */
+    text-align: center;
+    color: rgb(90,90,90);
+    width: 20%;
+}
+.data-item.releaseDate, .data-item.endDate {
+    /* background-color: purple; */
+    width: 25%;
+    text-align: right;
+}
+
+</style>
---
+++ src/components/movieManage.vue
@@ -0,0 +1,79 @@
+<script>
+import sideMenus from "./components/sideMenu.vue"
+import movieInfo from "./components/barComponet.vue"
+
+export default {
+  components: {
+    sideMenus,
+    movieInfo
+  },
+  data: () => {
+    return {
+      movies: [
+        {
+          name: "荒野之路",
+          duration: 105,
+          onDate: "2023-10-24",
+          offData: "2023-12-23"
+        },
+        {
+          name: "生化危机",
+          duration: 123,
+          onDate: "2024-7-3",
+          offData: "2023-10-2"
+        }
+      ],
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <sideMenus />
+
+  <main>
+    <div class="topbar">topBar</div>
+
+    <section class="content">
+      <div class="movieData">
+        <movieInfo v-for="movie in movies" :key="movie.name" :movieName="movie.name" :duration="movie.duration"
+          :releaseDate="movie.onDate" :endDate="movie.offData" />
+      </div>
+    </section>
+
+  </main>
+</template>
+
+<style scoped>
+main {
+  position: relative;
+  width: 84%;
+  left: 16%;
+
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+
+  /* background-color: aqua; */
+}
+
+.content {
+  margin: 0;
+  /* background-color: red; */
+  width: 80%;
+}
+.movieData {
+  margin: 5% 5%;
+  /* background-color: blueviolet; */
+}
+
+.topbar {
+  /* background-color: blue; */
+  height: 10vh;
+  text-align: center;
+}
+</style>
