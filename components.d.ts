585650e22a13b8a0cb56ca5becda1658984f0ba3
momoko <2602780256@qq.com>
2023-07-14 12:49:09 +0800

    修复小bug

--- components.d.ts
+++ components.d.ts
@@ -7,17 +7,30 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BarComponet: typeof import('./src/components/administrator/barComponet.vue')['default']
     ChooseActor: typeof import('./src/components/administrator/chooseActor.vue')['default']
+    CinemaInfo: typeof import('./src/components/administrator/cinemaInfo.vue')['default']
+    CinemaManage: typeof import('./src/components/administrator/cinemaManage.vue')['default']
     CustomerLogin: typeof import('./src/components/customer/CustomerLogin.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
+    ElDrawer: typeof import('element-plus/es')['ElDrawer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
+    ElSpace: typeof import('element-plus/es')['ElSpace']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    ElTag: typeof import('element-plus/es')['ElTag']
     HomePage: typeof import('./src/components/HomePage.vue')['default']
+    MainView: typeof import('./src/components/administrator/mainView.vue')['default']
+    MovieInfo: typeof import('./src/components/administrator/movieInfo.vue')['default']
     MovieManage: typeof import('./src/components/administrator/movieManage.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SideMenu: typeof import('./src/components/administrator/sideMenu.vue')['default']
+    StaffManage: typeof import('./src/components/administrator/staffManage.vue')['default']
+    TopBar: typeof import('./src/components/administrator/topBar.vue')['default']
   }
 }
