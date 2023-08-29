ad606ec8b05ff48651482116c7b8c35385ffa7fc
Zhang Yao <cineazhan@gmail.com>
2023-08-30 00:16:31 +0800

    修复后台管理相关bug

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -9,15 +9,41 @@ export default class Movie {
   releaseDate: string | null
   removalDate: string | null
 
-  public constructor (data:{movieId:string ,name:string ,duration:string ,instruction:string | null ,score:number | null ,postUrl:string | null ,tags:string | null ,releaseDate:string | null ,removalDate:string | null}) {
-    this.movieId = data.movieId
-    this.name = data.name
-    this.duration = data.duration
-    this.instruction = data.instruction || ''
-    this.score = data.score || 0
-    this.postUrl = data.postUrl || ''
-    this.tags = data.tags || ''
-    this.releaseDate = data.releaseDate
-    this.removalDate = data.removalDate
+  public constructor(
+    data:
+      | {
+          movieId: string
+          name: string
+          duration: string
+          instruction: string | null
+          score: number | null
+          postUrl: string | null
+          tags: string | null
+          releaseDate: string | null
+          removalDate: string | null
+        }
+      | undefined
+  ) {
+    if (data) {
+      this.movieId = data.movieId
+      this.name = data.name
+      this.duration = data.duration
+      this.instruction = data.instruction || ''
+      this.score = data.score || 0
+      this.postUrl = data.postUrl || ''
+      this.tags = data.tags || ''
+      this.releaseDate = data.releaseDate
+      this.removalDate = data.removalDate
+    } else {
+      this.movieId = ''
+      this.name = ''
+      this.duration = ''
+      this.instruction = null
+      this.score = null
+      this.postUrl = null
+      this.tags = null
+      this.releaseDate = null
+      this.removalDate = null
+    }
   }
 }
