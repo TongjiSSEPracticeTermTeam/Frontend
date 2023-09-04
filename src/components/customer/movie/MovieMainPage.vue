8e631911673a444a86fc8027f168de4ad9b9f497
luxingzhi27 <3523718637@qq.com>
2023-09-04 13:06:04 +0800

    将筛选tag的功能改为在前端执行

--- src/components/customer/movie/MovieMainPage.vue
+++ src/components/customer/movie/MovieMainPage.vue
@@ -2,16 +2,19 @@
 import {onMounted, ref, computed, watch} from "vue";
 import tagSelect from "@/components/customer/movie/AttributeSelect.vue"
 import type Movie from "@/models/Movie";
-import axios from "axios";
+import axios, {all} from "axios";
 import MovieCard from "@/components/customer/movie/MovieCard.vue";
 
 const tags=ref<string[]>([])
 const years=ref<string[]>([])
-years.value=['2023','2022','2021','2020','2019']
+years.value=['2023','2022','2021','2020','2019','2018','2017','2016','2015']
 const selectedTags=ref([])
 const selectedYears=ref([])
 
 const movies=ref<Movie[]>([])
+const currentClass=ref('1') //表示当前处于正在热映还是即将上映,1表示正在热映，2表示即将上映
+const onPlayMovies=ref<Movie[]>([])
+const commingSoonMovies=ref<Movie[]>([])
 const currentPage=ref(1)
 const pageSize=ref(6) //一页展示的电影数量
 
@@ -21,16 +24,13 @@ const totalMovies = computed(() => movies.value.length);
 
 const tagSelected=(t)=>{
   selectedTags.value=t
-  console.log(selectedTags.value)
   updateMovies()
 }
 
-watch(paginatedMovies,(newMovies)=>{
-  console.log(`new page movies:${newMovies}`)
-})
 
 const yearSelected=(t)=>{
   selectedYears.value=t
+  updateMoviesByYear()
 }
 const getAllTags=()=>{
   axios.get('/api/Movies/tags/getAllTags').then((r)=>{
@@ -40,23 +40,111 @@ const getAllTags=()=>{
   })
 }
 
-const updateMovies=()=>{
-  let params=new URLSearchParams()
-  selectedTags.value.forEach((v)=>{
-    params.append('tags',v)
-  })
-  axios.get('/api/Movies/tags',{
-    params: params
-  }).then((r)=>{
+const initMovies=()=>{
+  let allMovies=new Array<Movie>()
+  axios.get('/api/Movies/tags').then((r)=>{
     if(r.data&&r.data.status==='10000'){
-      movies.value=r.data.data
+      allMovies=r.data.data
+      if(allMovies.length){
+        allMovies.forEach((movie)=>{
+          if(isOnPlay(movie.releaseDate,movie.removalDate)){
+            onPlayMovies.value.push(movie)
+          }else if(isCommingSoon(movie.releaseDate)){
+            commingSoonMovies.value.push(movie)
+          }
+        })
+        if(currentClass.value==='1'){
+          movies.value=onPlayMovies.value
+        }else if(currentClass.value==='2'){
+          movies.value=commingSoonMovies.value
+        }
+      }
     }
   })
+
+
+}
+
+const updateMoviesByTag=()=>{
+  // let params=new URLSearchParams()
+  // selectedTags.value.forEach((v)=>{
+  //   params.append('tags',v)
+  // })
+  // axios.get('/api/Movies/tags',{
+  //   params: params
+  // }).then((r)=>{
+  //   if(r.data&&r.data.status==='10000'){
+  //     movies.value=r.data.data
+  //   }
+  // })
+  if(currentClass.value==='1'){
+    movies.value=onPlayMovies.value.filter((movie)=>{
+      let flag=true
+      selectedTags.value.forEach((tag)=>{
+        if(!movie.tags?.includes(tag)){
+          flag=false
+        }
+      })
+      return flag
+    })
+  }else if(currentClass.value==='2'){
+    movies.value=commingSoonMovies.value.filter((movie)=>{
+      let flag=true
+      selectedTags.value.forEach((tag)=>{
+        if(!movie.tags?.includes(tag)){
+          flag=false
+        }
+      })
+      return flag
+    })
+  }
+}
+
+const isOnPlay=(releaseDate:string|null,removalDate:string|null)=>{
+  if(releaseDate===null||removalDate===null){
+    return false
+  }
+  let now=new Date()
+  let release=new Date(releaseDate)
+  let removal=new Date(removalDate)
+  return (now.getTime()>release.getTime())&&(now.getTime()<removal.getTime())
+}
+
+const isCommingSoon=(releaseDate:string|null)=>{
+  if(releaseDate===null){
+    return false
+  }
+  let now=new Date()
+  let release=new Date(releaseDate)
+  return (now.getTime()<release.getTime())
+}
+
+const handleSelect=(index:string)=>{
+  if(index==='1'){
+    movies.value=onPlayMovies.value
+    currentPage.value=1
+  }else if(index==='2'){
+    movies.value=commingSoonMovies.value
+    currentPage.value=1
+  }
+  currentClass.value=index
+  updateMoviesByTag()
+}
+
+const updateMoviesByYear=()=>{
+  movies.value=movies.value.filter((movie)=>{
+    console.log(movie.releaseDate.substring(0,4))
+    return selectedYears.value.includes(movie.releaseDate.substring(0,4))
+  })
+}
+const updateMovies=()=>{
+  updateMoviesByTag()
+  // updateMoviesByYear()
 }
 
 onMounted(()=>{
   getAllTags()
-  updateMovies()
+  initMovies()
 })
 </script>
 
@@ -69,8 +157,12 @@ onMounted(()=>{
           <el-col :span="3"/>
           <el-col :span="18" class="translucent-card">
             <el-card>
+              <el-menu mode="horizontal" @select="handleSelect" default-active="1">
+                <el-menu-item index="1">正在热映</el-menu-item>
+                <el-menu-item index="2">即将上映</el-menu-item>
+              </el-menu>
               <tagSelect @selected="tagSelected" :label="'类型'" :tags="tags" ></tagSelect>
-              <tagSelect @selected="yearSelected" :label="'年份'" :tags="years" ></tagSelect>
+              <!-- <tagSelect @selected="yearSelected" :label="'年份'" :tags="years" ></tagSelect> -->
             </el-card>
           </el-col>
           <el-col :span="3"/>
