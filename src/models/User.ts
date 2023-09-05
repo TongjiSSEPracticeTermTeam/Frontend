0b96b37447786ebc6df9b65b0df1e03de52f1ab2
luxingzhi27 <3523718637@qq.com>
2023-09-05 20:28:22 +0800

    新建个人卡片

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,12 +2,16 @@ export default class User {
   id: string
   displayName: string
   avatarUrl: string
+  email: string
+  vip:string //vip结束日期
   type: 'Anonymous' | 'Customer' | 'Manager' | 'Administrator'
 
   constructor() {
     this.id = ''
     this.displayName = '未登录'
     this.avatarUrl = '/img/default_user_avatar.png'
+    this.email = ''
+    this.vip = ''
     this.type = 'Anonymous'
   }
 }
