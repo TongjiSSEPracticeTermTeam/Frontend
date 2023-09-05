92246cb34324cd932686cf03f62fb947274e8e1d
momoko <2602780256@qq.com>
2023-09-05 16:18:16 +0800

    初步完成影院界面的地区筛选，影院地区需要规范录制

---
+++ src/helpers/AreaSelect.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <el-cascader
+    size="default"
+    :options="pcaTextArr"
+    v-model="selectedOptions"
+    @change="handleChange">
+    </el-cascader>
+</div>
+</template>
+
+<script>
+import { pcaTextArr } from 'element-china-area-data'
+export default {
+data () {
+    return {
+    pcaTextArr,
+    selectedOptions: []
+    }
+},
+methods: {
+    handleChange() {
+    this.$emit('selected', this.selectedOptions)
+    }
+}
+}
+</script>
