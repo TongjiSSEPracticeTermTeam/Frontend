2fd1f53f32381a90c6755d72dfbb091fbbf91d2e
gyy <g18355735449@163.com>
2023-09-06 15:24:44 +0800

    uncompleted personal page

--- src/components/customer/person/PersonCard.vue
+++ src/components/customer/person/PersonCard.vue
@@ -7,14 +7,17 @@ import tinygradient from 'tinygradient'
 import { useStore } from 'vuex'
 import User from '@/models/User'
 
+import UploadImage from '@/helpers/UploadImage.vue'
+import type { FormInstance, FormRules } from 'element-plus'
+
 const store = useStore()
 
 
 
-const props=defineProps({
-  user:{
-    type:User,
-    required:true
+const props = defineProps({
+  user: {
+    type: User,
+    required: true
   }
 })
 
@@ -80,21 +83,105 @@ const avatarLoaded = () => {
     }
   })
 }
+
+
+
+let detailPerson = ref(false)
+let editStatus = ref(false)
+const savingDetail = ref(false)
+let formRef = ref<FormInstance | null>(null)
+
+
+const handleDrawerClose = () => {
+  if (editStatus.value) {
+    ElMessageBox.confirm('有未保存的数据，确定要退出编辑吗？', 'Warning', {
+      confirmButtonText: '确认',
+      cancelButtonText: '取消',
+      type: 'warning'
+    })
+      .then(() => {
+        formRef.value?.clearValidate()
+        detailPerson.value = false
+        editStatus.value = false
+      })
+      .catch(() => {
+      })
+  } else {
+    detailPerson.value = false
+    editStatus.value = false
+  }
+}
+
+const saveDetail = async () => {
+}
+
 </script>
 
 <template>
-    <el-card shadow="hover" class="avatar-card">
-        <div class="avatar-card-background" :style="`background: ${avatarCardBackground};`" />
-            <div class="avatar-card-content flex" style="display: flex;align-items: center;">
-                  <div class="person-avatar">
-                      <img id="avatar" :src="'https://cinemadb-1305284863.cos.accelerate.myqcloud.com/userdata/poster/27d257e3ba7c4d3b81ad144f9b304975.jpg'" alt="" @load="avatarLoaded" />
-                  </div>
-                  <div class="ml-10" :style="`display: flex; flex-direction:column; color: ${avatarCardFontColor}`">
-                    <h1 class="text-4xl font-extrabold">{{ user.displayName }}</h1>
-                    <h3 class="text-1xl font-light my-2.5">欢迎来到同济院线</h3>
-                  </div>
-            </div>
-    </el-card>
+  <el-card shadow="hover" class="avatar-card" @click="()=>{detailPerson = true}">
+    <div class="avatar-card-background" :style="`background: ${avatarCardBackground};`" />
+    <div class="avatar-card-content flex" style="display: flex;align-items: center;">
+      <div class="person-avatar">
+        <img id="avatar"
+          :src="'https://cinemadb-1305284863.cos.accelerate.myqcloud.com/userdata/poster/27d257e3ba7c4d3b81ad144f9b304975.jpg'"
+          alt="" @load="avatarLoaded" />
+      </div>
+      <div class="ml-10" :style="`display: flex; flex-direction:column; color: ${avatarCardFontColor}`">
+        <h1 class="text-4xl font-extrabold">{{ user.displayName }}</h1>
+        <h3 class="text-1xl font-light my-2.5">欢迎来到同济院线</h3>
+      </div>
+    </div>
+  </el-card>
+
+  <el-drawer v-model="detailPerson" title="个人主页" direction="rtl" :before-close="handleDrawerClose" style="min-width: 500px">
+    <el-form :model="user" label-width="120px" :rules="rules" ref="formRef">
+      
+      <el-form-item label="头像" class="w-full">
+        <el-space direction="vertical" alignment="normal" wrap>
+          <el-input
+            v-model="user.avatarUrl"
+            :rows="3"
+            type="textarea"
+            style="width: 350px"
+            @change="editStatus = true"
+          />
+          <el-image
+            :src="user.avatarUrl"
+            :fit="'contain'"
+            style="height: 300px; width: 300px"
+          >
+            <template #error>
+              <el-icon>
+                <Picture />
+              </el-icon>
+            </template>
+          </el-image>
+          <UploadImage
+            api-path="/api/Customer/poster"
+            @Success="(Url: string) => {
+              user.avatarUrl = Url
+              editStatus = true
+            }"
+          />
+        </el-space>
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-input  v-model="user.displayName" @change="()=>{editStatus=true}"/>
+      </el-form-item>
+
+      <el-form-item label="邮箱">
+        <el-input v-model="user.email" @change="()=>{editStatus=true}"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="saveDetail" v-loading="savingDetail">保存</el-button>
+        <el-button @click="handleDrawerClose">取消</el-button>
+      </el-form-item>
+
+    </el-form>
+  </el-drawer>
+
+  
 </template>
 
 <style scoped lang="scss">
